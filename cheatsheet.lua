ea3bc1c0913cb2501577d8572355cf6a8f62ad34
Lorenzo Turrino <neuronefunzionante@gmail.com>
2018-03-25 00:48:39 +0000

    add more functions and some tables

--- cheatsheet.lua
+++ cheatsheet.lua
@@ -32,8 +32,9 @@ for num = 0, 10, 1 do -- inclusive range with step loop
     num = num + 1
 end
 
+-- while looping
 local iter = 0
-while iter < 1 do -- while looping
+while iter < 1 do
     iter = iter + 1
 end
 
@@ -41,10 +42,37 @@ end
 print('waiting...')
 os.execute("sleep " .. tonumber(0.5) ) -- non-busy sleeping with a os call (*nix only) and explicit num conversion
 
--- function def
+-- first class functions
 function pong(ping)
     return tostring(ping) .. ' pong!'
 end
 print(pong('ping!'))
 
--- anonymous functions and closures
+-- closures
+function closured(divisor)
+    return function(to_divide) return to_divide / divisor end --inline declaration
+end
+print(closured(10)(100))
+
+-- recursion with anonymous function and local var. This sounds like a easy to break idea.
+recur = function(number)
+    print('counting down ' .. number)
+    if number <= 0 then print('done.') return end
+    return recur(number - 1)
+end
+recur(15)
+
+-- tables with js-like acces
+local tab = {
+    k1 = 'one',
+    k2 = 2,
+}
+print('non existing key: ' .. tostring(tab.nope))
+tab['ba'] = 'nana'  -- literal notation
+tab.k2 = nil -- nil value is equivalent to non-existing key.
+for key, val in pairs(tab) do
+    print(key, val)
+end
+
+
+-- also indexes start at 1
